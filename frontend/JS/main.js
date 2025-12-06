3bb2deeffdc6ab4262d9fcf737092157ae20ce18
Evitalires <evitalires@gmail.com>
2025-12-05 22:34:28 -0500

    Uniendo aporte de Lili

--- frontend/JS/main.js
+++ frontend/JS/main.js
@@ -5,19 +5,9 @@ import { mostrarHeader } from "./modulos/header.js";
 import { headLinks } from "./modulos/headLinks.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-headLinks();
-mostrarHeader();
-<<<<<<< HEAD
-mostrarFooter();
-
-
-const carrito = new Carrito("abrirCarrito");
-
-carrito.inicializar();
-
+    headLinks();
+    mostrarHeader();
+    mostrarFooter();
+    const carrito = new Carrito("abrirCarrito");
+    carrito.inicializar();
 });
-=======
-
-const carrito = new Carrito("abrirCarrito");
-carrito.inicializar();
->>>>>>> 2204b37573b16526215fdf65e5ce6ae0432df048

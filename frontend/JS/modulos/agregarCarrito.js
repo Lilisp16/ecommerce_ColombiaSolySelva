4b8c00c1116d0f7339c94e40c2510f9d0b661c16
dmoyamur <dmoya844@gmail.com>
2025-12-11 11:53:34 -0500

    Funcionalidad + y -

--- frontend/JS/modulos/agregarCarrito.js
+++ frontend/JS/modulos/agregarCarrito.js
@@ -13,7 +13,9 @@ function guardarCarrito(){
 export function actualizarBadgeCarrito() {
     const cantidad = carrito.reduce((total, item) => total + item.cantidad, 0);;
     const cartCount = document.getElementById("cartCount");
+    const totalItems = document.getElementById("totalItemCount")
     if (cartCount) cartCount.textContent = cantidad
+    if (totalItems) totalItems.textContent = cantidad
 }
 
 export const mostrarCarrito = () => {
@@ -22,33 +24,40 @@ export const mostrarCarrito = () => {
 
   /* nombree,precio,cantidad,imagen */
   carrito.forEach((item) => {
-    /* contenedor.innerHTML += `
-      <div class="d-flex justify-content-between align-items-center border-bottom py-2">
-        <div class="d-flex align-items-center gap-2">
-          <img 
-            src="${item.imagen}" 
-            style="width: 50px; height: 50px; object-fit: cover; border-radius: 5px;"
-          >
-          <div>
-            <h6 style="color:black"><strong>${item.nombre}</strong></h6>
-            <h6 style="color:black"><strong>$${item.precio}</strong></h6>
-            <h6 style="color:black"><strong>${item.cantidad}</strong></h6>
-          </div>
-        </div>
-      </div>
-    `; */
     contenedor.innerHTML += cartItemCarrito(item);
+  });
+
+   contenedor.querySelectorAll(".btn-incrementar").forEach(btn => {
+    btn.addEventListener("click", () => {
+      const id = parseInt(btn.dataset.id);
+      const producto = carrito.find(p => p.id === id);
+      if (producto) {
+        producto.cantidad = (producto.cantidad || 1) + 1;
+        guardarCarrito();
+        actualizarBadgeCarrito();
+        mostrarCarrito(); // volver a renderizar
+      }
+    });
+  });
+
+    contenedor.querySelectorAll(".btn-decrementar").forEach(btn => {
+    btn.addEventListener("click", () => {
+      const id = parseInt(btn.dataset.id);
+      const producto = carrito.find(p => p.id === id);
+      if (producto) {
+        producto.cantidad = (producto.cantidad || 1) - 1;
+        guardarCarrito();
+        actualizarBadgeCarrito();
+        mostrarCarrito(); // volver a renderizar
+      }
+    });
   });
 };
 
+let productosLocalStorage = JSON.parse(localStorage.getItem("productos")) || [];
+console.log("PRODUCTOS DEL LOCALSTORAGE:", productosLocalStorage);
 
-    
-
-    let productosLocalStorage = JSON.parse(localStorage.getItem("productos")) || [];
-    console.log("PRODUCTOS DEL LOCALSTORAGE:", productosLocalStorage);
-
-
-    export const agregarAlCarrito = (producto) => {
+export const agregarAlCarrito = (producto) => {
     console.log("Producto recibido:", producto);
     console.log("Carrito antes:", carrito);
 
@@ -73,27 +82,27 @@ export const mostrarCarrito = () => {
         showConfirmButton: true,
     });
     console.log("Carrito después:", carrito);
-    };
+};
 
-    export function eliminarDelCarrito(id){
+export function eliminarDelCarrito(id){
         carrito = carrito.filter((item) => item.id !== id);
         localStorage.setItem("carrito", JSON.stringify(carrito));
         guardarCarrito();
         actualizarBadgeCarrito();
         mostrarCarrito();
   
-    };
+};
 
-
-    export const obtenerCantidadTotal = () => {
+export const obtenerCantidadTotal = () => {
         const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
         return carrito.reduce((total, item) => total + item.cantidad, 0);
-    };
+};
 
-    window.addEventListener("load", () => {
+window.addEventListener("load", () => {
     mostrarCarrito();
     actualizarBadgeCarrito();
-    });
+});
     
+
 
 
--- frontend/JS/modulos/cartItem.js
+++ frontend/JS/modulos/cartItem.js
@@ -21,18 +21,15 @@ export function cartItemCarrito (producto) {
                             <h5 class="card-title mb-0 fw-normal text-truncate text-nowrap d-block" style="max-width: 150px;">
                                 ${producto.nombre}asdfasdfasdfasdfasdfasdf
                             </h5>
-                            <p class="card-text mb-0">
-                            <small class="text-secondary fw-bold">En stock</small>
-                            </p>
-                            <p class="card-text fw-bold fs-5">$ ${producto.precio} </p>
+                            <h6>Vr. Unit. $ ${producto.precio} </h6>
                         </div>
 
                         <div class="d-flex flex-column align-items-end justify-content-between" style="height: 80px;">
                             <i class="bi bi-heart-fill"></i>
                             <div class="d-flex align-items-center gap-2">
-                            <button class="text-black btn btn-outline-dark btn-sm p-0 rounded-2 d-flex justify-content-center align-items-center" style="width: 25px; height: 25px;">-</button>
-                            <span class="fw-bold">${producto.stock}</span>
-                            <button class="text-black btn btn-outline-dark btn-sm p-0 rounded-2 d-flex justify-content-center align-items-center" style="width: 25px; height: 25px;">+</button>
+                            <button class="text-black btn btn-outline-dark btn-sm p-0 rounded-2 d-flex justify-content-center align-items-center btn-decrementar" data-id="${producto.id}" style="width: 25px; height: 25px;">-</button>
+                            <span class="fw-bold">${producto.cantidad}</span>
+                            <button class="text-black btn btn-outline-dark btn-sm p-0 rounded-2 d-flex justify-content-center align-items-center btn-incrementar" data-id="${producto.id}" style="width: 25px; height: 25px;">+</button>
                             </div>  
                         </div>
 
@@ -69,6 +66,53 @@ export const productosRecientes = (producto) => {
                         </div>
                         <div class="d-flex flex-column align-items-end justify-content-between" style="height: 80px;">
                             <i class="bi bi-heart-fill"></i>
+                        </div>
+
+                        </div>
+                    </div>
+                </div>
+              </div>
+    `
+    return nuevoProducto;
+} 
+
+export function cartItemCarritoVersion1 (producto) {
+    const nuevoProducto = `
+    <div class="d-flex justify-content-center">
+                <div class="cartItem card rounded-4 mb-3" style="max-width: 500px;">
+                    <div class="card-body  rounded-4 border border-dark p-3 text-dark">
+                        <div class="d-flex align-items-center">
+                        <div class="me-2 d-flex flex-column gap-4">
+                            <label class="checkbox-wrapper">
+                                <input type="checkbox">
+                                <span class="checkmark"></span>
+                            </label>
+                            <i class="bi bi-trash3"></i>
+                        </div>
+                        <div class="me-3">
+                            <div class="border border-black bg-dark rounded-4" style="width: 80px; height: 80px;">
+                                <img src="${producto.imagen}" class="img-fluid h-100 object-fit-cover">
+                            </div>
+                        </div>
+
+                        <div class="flex-grow-1">
+                            <h5 class="card-title mb-0 fw-normal text-truncate text-nowrap d-block" style="max-width: 150px;">
+                                ${producto.nombre}asdfasdfasdfasdfasdfasdf
+                            </h5>
+                            <p class="card-text mb-0">
+                            <small class="text-secondary fw-bold">En stock</small>
+                            <small class="fw-bold">${producto.stock} unds</small>
+                            </p>
+                            <h6>Vr. Unit. $ ${producto.precio} </h6>
+                        </div>
+
+                        <div class="d-flex flex-column align-items-end justify-content-between" style="height: 80px;">
+                            <i class="bi bi-heart-fill"></i>
+                            <div class="d-flex align-items-center gap-2">
+                            <button class="text-black btn btn-outline-dark btn-sm p-0 rounded-2 d-flex justify-content-center align-items-center btn-decrementar" data-id="${producto.id}" style="width: 25px; height: 25px;">-</button>
+                            <span class="fw-bold">${producto.cantidad}</span>
+                            <button class="text-black btn btn-outline-dark btn-sm p-0 rounded-2 d-flex justify-content-center align-items-center btn-incrementar" data-id="${producto.id}" style="width: 25px; height: 25px;">+</button>
+                            </div>  
                         </div>
 
                         </div>
